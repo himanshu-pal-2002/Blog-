f67f0cc84d615ba280a562971cd81056c96beca9
Himanshu Pal <palhimanshu206243@gmail.com>
2023-12-27 12:04:54 +0530

    blog

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import *
 
-# Create your views here.
-def Home(request):
-    return render(request,'home.html')
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+    

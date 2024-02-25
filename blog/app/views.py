2d665f32d163c63246f0725397e5bfa1eee53ae8
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-25 23:00:35 +0530

    blog

--- blog/app/views.py
+++ blog/app/views.py
@@ -4,30 +4,32 @@ from django.views.generic import ListView,DetailView,CreateView,UpdateView,Delet
 from .models import Post
 from .forms import PostForm,EditForm
 
-# Views For Listview:
+# Classbased Views For Listview:
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
 
-# Views For DetailView:
+# Classbased Views For DetailView:
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
     
-# Views For CreateView:
+# Classbased Views For CreateView:
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
-    
+
+# Classbased Views For UpdateView:
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
     # fields = ['title','title_tag','body']
 
+# Classbased Views For Deleteview:
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'

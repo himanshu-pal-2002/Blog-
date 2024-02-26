461c05b7fa7c8917322886331d68d76a0054a2d9
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-26 23:58:26 +0530

    blog

--- blog/app/views.py
+++ blog/app/views.py
@@ -20,7 +20,6 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    # fields = '__all__'
 
 # Classbased Views For UpdateView:
 class UpdatePostView(UpdateView):

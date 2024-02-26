df0200b6bedf09a5171b16806afefac8348c6827
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-26 23:55:22 +0530

    blog

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,re_path
 from .views import *
 
 urlpatterns = [
-
     # path('register/',UserRegisterView.as_view(),name='register'),  
     path('logout/',logout_user, name='logout'),
     path('login/',login_user, name='login'),

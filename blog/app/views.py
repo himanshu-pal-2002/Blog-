cb8d2b0dbb8b84ad02e6d3e2de981492456d45f3
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-28 11:41:35 +0530

    blog

--- blog/app/views.py
+++ blog/app/views.py
@@ -10,10 +10,16 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+
+# Function Based View for CategoryView:
 def CategoryView(request, cats):
-
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
-
     return render(request,'categories.html', {'cats':cats.title().replace('-',' '),'category_posts':category_posts})
 
 # Classbased Views For DetailView:

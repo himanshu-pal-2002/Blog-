3b0e8535ad1f000037ef9aac30825464ebb29ae4
Himanshu Pal       (Full Stack Developer) <palhimanshu206243@gmail.com>
2024-02-29 00:14:56 +0530

    blog

--- blog/app/forms.py
+++ blog/app/forms.py
@@ -18,9 +18,9 @@ class PostForm(forms.ModelForm):
             
             'title' : forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
-            'author' : forms.TextInput(attrs={'class': 'form-control'}),
+            'author' : forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
 
-            # 'author' : forms.Select(attrs={'class': 'form-control'}),
+            # 'author' : forms.Select(attrs={'class': 'form-control'}),'
             'category' : forms.Select(choices=choice_list ,attrs={'class': 'form-control'}),
             'body' : forms.Textarea(attrs={'class': 'form-control'}),
         }
